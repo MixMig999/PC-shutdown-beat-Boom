160e3fa6fa744c92253be1a8c84d2dc1116cfd34
MixMig999 <81419500+MixMig999@users.noreply.github.com>
2022-09-14 20:24:08 +0200

    Update Vypnout.bat

--- Vypnout.bat
+++ Vypnout.bat
@@ -4,4 +4,4 @@ PowerShell.exe Write-Output 'This program will shutdown this PC WITHOUT saving y
 timeout -1
 "Vengaboys - Boom, Boom, Boom, Boom.mp3"
 timeout 17
-//shutdown /s /f /t 0
+shutdown /s /f /t 0
