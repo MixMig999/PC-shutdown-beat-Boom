3ea0beb8df880d43a1a3c621bf696558c5ffe7c2
MixMig999 <81419500+MixMig999@users.noreply.github.com>
2022-09-14 19:10:04 +0200

    Add files via upload

---
+++ Vypnout.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+color d
+PowerShell.exe Write-Output 'This program will shutdown this PC WITHOUT saving your programs.'
+timeout -1
+"Vengaboys - Boom, Boom, Boom, Boom.mp3"
+timeout 17
+//shutdown /s /f /t 0
